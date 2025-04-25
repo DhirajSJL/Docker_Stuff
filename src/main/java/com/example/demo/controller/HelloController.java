c985192b7fa849a54a8980ec7fba842aefd19cc4
Dhirajkumar <dhiraj6.lulla@rediffmail.com>
2025-04-25 14:55:54 +0530

    Change in Hello message

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -9,6 +9,7 @@ public class HelloController {
     @GetMapping("/")
     public String hello() {
         //return "Hello from Spring Boot + PostgreSQL!"
-        return "Hello from Dhirajkumar Lulla + DB tech!";
+        //return "Hello from Dhirajkumar Lulla + DB tech!";
+        //return "Hello from Project which executes Docker with Jenkins ";
     }
 }
